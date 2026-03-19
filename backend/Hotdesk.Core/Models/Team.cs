8d1b702df136c01ca1b4d86cb553a96873dcf4dd
oreoredstone <mr.oreoredstone@gmail.com>
2026-03-19 23:09:46 +0000

    Added Core project

---
+++ backend/Hotdesk.Core/Models/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hotdesk.Core.Models;
+
+public class Team
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int DepartmentId { get; set; }
+
+    public virtual Department Department { get; set; } = null!;
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
